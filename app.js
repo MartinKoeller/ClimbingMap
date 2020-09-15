90af6751bcaa4e1fc62bac5d5c33e91d2ad09903
mfbehrens99 <58851724+mfbehrens99@users.noreply.github.com>
2020-09-15 14:57:11 +0200

    data-display

--- app.js
+++ app.js
@@ -26,10 +26,18 @@
 	});
 
 	var outdoorIcon = new LeafIcon({iconUrl: 'outdoor.png'});
+	var outdoorGreyIcon = new LeafIcon({iconUrl: 'outdoor_grey.png'});
 	var buildingIcon = new LeafIcon({iconUrl: 'building.png'});
 
 	var markers = L.markerClusterGroup({ chunkedLoading: true });
-
+	
+	var styleTags = new Map([["climbing:boulder","boulder"],["climbing:sport","sport"],["climbing:speed","speed"],["climbing:toprope","toprope"],["climbing:trad","traditional"],["climbing:multipitch","multi pitch"],["climbing:ice","ice climbing"],["climbing:mixed","mixed"],["climbing:deepwater","deepwater"]]);
+	var hideTags = ["name","sport"];
+	hideTags = hideTags.concat(Array.from(styleTags.keys()));
+	hideTags = hideTags.concat(["climbing:length","climbing:length:min","climbing:length:max"]);
+	hideTags = hideTags.concat(["climbing:grade:uiaa","climbing:grade:uiaa:min","climbing:grade:uiaa:max","climbing:grade:uiaa:mean"]);
+	hideTags = hideTags.concat(["website","url","wikipedia","wikidata"]);
+	
 	  //alert(JSON.stringify(addressPoints.features[2]));
 	for (var i = 0; i < addressPoints.elements.length; i++) {
 	//for (var i = 0; i < 720; i++) {
@@ -38,7 +46,6 @@
 		var  lat = 52.1;
 		var  lng = 8.7;
 		var  tags = "";
-		var  isCool = false;
 		var  isClimbing = true;
 
 		if (a.tags) {
@@ -61,31 +68,90 @@
 			}
 			
 		
-			//tags =JSON.stringify(a.tags);
-			for (var tag in a.tags) {
-				tags += "<br>" +tag +"="+a.tags[tag];
-				if (tag.search(/climbing.+/) != -1){
-				   title = "*" + title;
-				   isCool=true;
-				   if (tag =="climbing:sport" && a.tags[tag]=="no"){
-					 isClimbing = false;
-					}
+			// Print details
+			
+			// Climbing style
+			var styles = [];
+			for (var [tag,value] of styleTags) {
+				if (a.tags[tag] == "yes" || a.tags[tag] == "only") {
+					styles.push(value);
 				}
-											
 			}
+			if (styles.length > 0){tags += "Climbing styles: " + styles.join(", ");}
 			
 			
-			if (title == "unnamed") {
-			 isCool=false;
-			
+			//UIAA
+			var plain = a.tags["climbing:grade:uiaa"];
+			var min = a.tags["climbing:grade:uiaa:min"];
+			var max = a.tags["climbing:grade:uiaa:max"];
+			var mean = a.tags["climbing:grade:uiaa:mean"];
+			if (plain) {tags += "</br>UIAA grade: " + plain;}
+			else if (min || max) {
+				tags += "</br>UIAA grade: ";
+				if (min && max) { tags += min + " - " + max;}
+				else if (min) {tags += min + " and harder";}
+				else {tags += "up to " + max;}
+				if (mean) {tags += ", mean " + mean;}
 			}
+		
+			
+			//Hight
+			var length = a.tags["climbing:length"];
+			var lengthmin = a.tags["climbing:length:min"];
+			var lengthmax = a.tags["climbing:length:max"];
+			
+			if (lengthmin || lengthmax) {
+				tags += "</br>Length: ";
+				if (lengthmin && lengthmax) { tags += lengthmin + " - " + lengthmax + "m";}
+				else if (lengthmin) {tags += lengthmin  + "m and higher";}
+				else {tags += "up to " + lengthmax + "m";}
+			} else if (length){ tags += "</br>Length: " + length + "m";}
+			
+			
+			
+			//Internet
+			var internet = []
+			var web = a.tags["website"];
+			var url = a.tags["url"];
+			var wiki = a.tags["wikipedia"];
+			
+			if (web) {internet.push("<a target='_blank' href=\"" + web + "\">official website</a>");}
+			if (url) {internet.push("<a target='_blank' href=\"" + url + "\">additional information</a>");}
+			if (wiki) {internet.push("<a target='_blank' href=\"https://en.wikipedia.org/wiki/" + wiki + "\">Wikipedia</a>");}
+			if (internet.length > 0) {tags += "</br>Links: " + internet.join(", ");}
+			
+			
+			//Indoor, Outdoor
+			var outdoor = a.tags["outdoor"];
+			var indoor = a.tags["indoor"];
+			if (outdoor == "yes"){}
+			
+			
+			var osm = [];
+			for (var tag in a.tags) {
+				if (!hideTags.includes(tag)){
+					osm.push(tag +"="+a.tags[tag]);
+					if (tag.search(/climbing.+/) != -1){
+					   if (tag =="climbing:sport" && a.tags[tag]=="no"){
+						 isClimbing = false;
+						}
+					}
+				}						
+			}
+			if (osm.length > 0) {tags += "</br></br> Other OSM tags:</br>" + osm.join("</br>");}
+			
 
 			if ( isClimbing) {
 				var marker;
 				if (isBuilding(a)) {
 					marker = L.marker(L.latLng(lat, lng), { title: title  ,icon: buildingIcon });
 				} else {
-					marker = L.marker(L.latLng(lat, lng), { title: title  ,icon: outdoorIcon });
+					// Grey out closed and private climbing sites
+					if (a.tags.access == "private" || a.tags.access == "no") {
+						marker = L.marker(L.latLng(lat, lng), { title: title  ,icon: outdoorGreyIcon });
+					} else {
+						marker = L.marker(L.latLng(lat, lng), { title: title  ,icon: outdoorIcon });
+					}
 				}
 				marker.bindPopup("<h2>"+title+"</h2>"+tags);
 				markers.addLayer(marker);
