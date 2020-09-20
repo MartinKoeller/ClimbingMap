212e7a3e20944c25942cb598143d71d1ad7616c3
mfbehrens99 <58851724+mfbehrens99@users.noreply.github.com>
2020-09-20 20:51:54 +0200

    automatic update script for windows

--- getData.bat
+++ getData.bat
@@ -1,11 +1,16 @@
 @echo off 
-set path=..\wget\
+rem set path=..\wget\
+
+git.exe checkout data-update
+git.exe fetch
 
 wget -O target1.osm "http://overpass-api.de/api/interpreter?data=[out:json];node[\"sport\"~\"climbing\"];out;way[\"sport\"~\"climbing\"];out;>;out;"
 
 echo var addressPoints= >data1.js
 type target1.osm >>data1.js
 echo ; >>data1.js
+del target1.osm
+git.exe add data1.js
 
 
 wget -O target2.osm "http://overpass-api.de/api/interpreter?data=[out:json];node[\"climbing:sport\"];out;way[\"climbing:sport\"];out;>;out;"
@@ -13,4 +18,9 @@ wget -O target2.osm "http://overpass-api.de/api/interpreter?data=[out:json];node
 echo var addressPoints= >data2.js
 type target2.osm >>data2.js
 echo ; >>data2.js
+del target2.osm
+git.exe add data2.js
+
+git.exe commit -m "updated map data %date:~-4%-%date:~3,2%-%date:~0,2% %time:~0,2%:%time:~3,2%:%time:~6,2%"
+
 rem pause
